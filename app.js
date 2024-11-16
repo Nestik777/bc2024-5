7b442afb77f4569d38323f653b5cb3b8d0086ddc
Nestor bc2024-5 <nestor.kuleba189@gmail.com>
2024-11-16 22:51:41 +0200

    do 1 part

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
 const express = require('express');
+const { Command } = require('commander');
 const app = express();
+const program = new Command();
 
+// Налаштування параметрів командного рядка
+program
+  .requiredOption('-h, --host <string>', 'server host address')
+  .requiredOption('-p, --port <number>', 'server port')
+  .requiredOption('-c, --cache <path>', 'path to cache directory')
+  .parse(process.argv);
+
+// Отримуємо значення параметрів
+const options = program.opts();
+const { host, port, cache } = options;
+
+// Перевіряємо шлях до кешу (можна додати перевірку існування директорії)
+console.log(`Cache directory: ${cache}`);
+
+// Налаштовуємо сервер Express
 app.get('/', (req, res) => {
-    res.send('Hello, World!');
+  res.send(`Server running on ${host}:${port}, caching to ${cache}`);
 });
 
-const PORT = 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+// Запускаємо сервер
+app.listen(port, host, () => {
+  console.log(`Server is running at http://${host}:${port}`);
 });
